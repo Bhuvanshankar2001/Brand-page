83e9803d084769962f4a987196ad636f7e8e7113
BhuvanshankarGithub <bhuvanshankargithub@gmail.com>
2023-07-23 15:53:11 +0530

    My first react project

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,21 @@
+
+
+const Navigation = () => {
+    return (
+        <div>
+            <nav className="container">
+                <div className="logo">
+                    <img src="/images/brand_logo.png" alt="" />
+                </div>
+                <ul>
+                    <li>Home</li>
+                    <li>Location</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
+                <button>Login</button>
+            </nav></div>
+    )
+}
+
+export default Navigation
